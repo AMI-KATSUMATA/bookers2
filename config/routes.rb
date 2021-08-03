7430ec24fa67053e94d982cac2c61f64ac923c56
AMI-KATSUMATA <amic2264@gmail.com>
2021-08-03 14:42:39 +0000

    [add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search' => 'searches#search'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
